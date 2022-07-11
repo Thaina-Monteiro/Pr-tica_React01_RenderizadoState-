30638d373abc191643fe78c28785867e7712d712
Thainá Monteiro <96400582+Thaina-Monteiro@users.noreply.github.com>
2022-07-11 01:50:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import fruta from './imagens/fruta.jpg';
+import React, { Component } from "react"
+
+
+export default class App extends Component{
+
+  state={
+    nome:'Thainá Monteiro',
+    idade: 23,
+    comidaFavorita: 'Pizza',
+    musicas:["Don't stop believin","Sol de paz", "Vienna"]
+  }
+  render(){
+    return(
+      <div>
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comidaFavorita}</h3>
+        <ul>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li>{this.state.musicas[2]}</li>
+        </ul>
+        <img src={fruta} alt="maçã" />
+        
+      </div>
+    )
+  }
 }
-
-export default App;
